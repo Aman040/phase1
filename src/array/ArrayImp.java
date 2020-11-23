38745a5c10984c2fc6a052914665fdebf38bbcdd
Aman Chaurasia <amanchaurasia040@gmail.com>
2020-11-24 00:38:51 +0530

    Arrays

---
+++ src/array/ArrayImp.java
@@ -0,0 +1,14 @@
+package array;
+
+public class ArrayImp {
+
+	public static void main(String[] args) {
+		int a[]= {10,20,30,40};
+		for(int i:a) {
+			System.out.println(i);
+		}
+		int b[][]= {{2,4,8,9},{2,4,6}};
+		System.out.println("\nLength of row 1: "+b[0].length);
+	}
+
+}
